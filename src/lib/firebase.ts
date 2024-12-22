84cc0240add625875925e9d6151e9bd81f50fdf4
Abishkar dhenga <Aabiskardhenga29@gmai.com>
2024-12-23 00:23:48 +0545

    removed firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,22 +2,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDZqexG1D16jZabqq2Fc6NXFrf24ojcBYc",
-//   authDomain: "bolt-1447a.firebaseapp.com",
-//   projectId: "bolt-1447a",
-//   storageBucket: "bolt-1447a.appspot.com",
-//   messagingSenderId: "315049398775",
-//   appId: "1:315049398775:web:97b68169d5f956b1e01ef2"
-// };
+
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAgdALOGjq7ziQ3vnyhapwnezC--iHglFk",
-  authDomain: "note-library-3f0e9.firebaseapp.com",
-  projectId: "note-library-3f0e9",
-  storageBucket: "note-library-3f0e9.firebasestorage.app",
-  messagingSenderId: "613865924191",
-  appId: "1:613865924191:web:6693d1c21ea787fa56ddea",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 const app = initializeApp(firebaseConfig);
