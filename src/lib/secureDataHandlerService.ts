1dd7462e1cc3882ef78ffbdccb93a7deea377f71
Abishkardhenga <copywriterayan47@gmail.com>
2024-12-23 01:13:42 +0545

    encrypt and decrypt made some changes

--- src/lib/secureDataHandlerService.ts
+++ src/lib/secureDataHandlerService.ts
@@ -1,16 +1,21 @@
-export const encryptOTP = (otp:string, secretKey:string) => {
-     let encryptedOtp = '';
+export const encryptOTP = (otp: string, secretKey: string): string => {
+  let encryptedOtp = '';
   for (let i = 0; i < otp.length; i++) {
     encryptedOtp += String.fromCharCode(otp.charCodeAt(i) ^ secretKey.charCodeAt(i % secretKey.length));
   }
-  return btoa(encryptedOtp); //
-}
+  return btoa(encryptedOtp);
+};
 
-export const decryptOTP = (encryptedOtp:string, secretKey:string)=> {
-  const decodedOtp = atob(encryptedOtp); // Decode the Base64 string
-  let otp = '';
-  for (let i = 0; i < decodedOtp.length; i++) {
-    otp += String.fromCharCode(decodedOtp.charCodeAt(i) ^ secretKey.charCodeAt(i % secretKey.length));
+export const decryptOTP = (encryptedOtp: string, secretKey: string): string => {
+  try {
+    const decodedOtp = atob(encryptedOtp); // Decode the Base64 string
+    let otp = '';
+    for (let i = 0; i < decodedOtp.length; i++) {
+      otp += String.fromCharCode(decodedOtp.charCodeAt(i) ^ secretKey.charCodeAt(i % secretKey.length));
+    }
+    return otp;
+  } catch (error) {
+    console.error('Error decoding OTP:', error);
+    throw new Error('Invalid encrypted OTP');
   }
-  return otp;
-}
+};
